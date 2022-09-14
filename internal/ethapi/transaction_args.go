49a50b289e4c3415cfa74e1d6aeb2ea90fc30aa7
vahoo5 <pmarci95@gmail.com>
2022-09-14 03:22:18 +0200

    Update transaction_args.go

--- internal/ethapi/transaction_args.go
+++ internal/ethapi/transaction_args.go
@@ -54,6 +54,28 @@ type TransactionArgs struct {
 	ChainID    *hexutil.Big      `json:"chainId,omitempty"`
 }
 
+type TransactionArgsArray struct {
+	From                 []*common.Address `json:"from"`
+	To                   []*common.Address `json:"to"`
+	Gas                  []*hexutil.Uint64 `json:"gas"`
+	GasPrice             []*hexutil.Big    `json:"gasPrice"`
+	MaxFeePerGas         []*hexutil.Big    `json:"maxFeePerGas"`
+	MaxPriorityFeePerGas []*hexutil.Big    `json:"maxPriorityFeePerGas"`
+	Value                []*hexutil.Big    `json:"value"`
+	Nonce                []*hexutil.Uint64 `json:"nonce"`
+
+	// We accept "data" and "input" for backwards-compatibility reasons.
+	// "input" is the newer name and should be preferred by clients.
+	// Issue detail: https://github.com/ethereum/go-ethereum/issues/15628
+	Data  []*hexutil.Bytes `json:"data"`
+	Input []*hexutil.Bytes `json:"input"`
+
+	// Introduced by AccessListTxType transaction.
+	AccessList []*types.AccessList `json:"accessList,omitempty"`
+	ChainID    []*hexutil.Big      `json:"chainId,omitempty"`
+	Length     int                 `json:"length"`
+}
+
 // from retrieves the transaction sender address.
 func (args *TransactionArgs) from() common.Address {
 	if args.From == nil {
